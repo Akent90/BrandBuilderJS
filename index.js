173d6c57867ede8f5c499790cde21aabfaaaa9ed
akent <alexanderkent099@gmail.com>
2024-02-01 15:06:37 -0500

    Correcting syntax on genereateSVG function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const { Circle, Triangle, Square } = require('./lib/shapes');
+const { Circle, Triangle, Square } = require('lib/shapes.js');
 
 async function promptUser() {
     return inquirer.prompt([
@@ -64,7 +64,7 @@ function saveSVGToFile(svgString) {
 async function main() {
     try {
         const userInput = await promptUser();
-        const svg = generateSvg(userInput.shape, userInput.shapeColor, userInput.text, userInput.textColor);
+        const svg = generateSVG(userInput.shape, userInput.shapeColor, userInput.text, userInput.textColor);
         saveSVGToFile(svg);
     } catch (error) {
         console.error('An error occurred:', error);
