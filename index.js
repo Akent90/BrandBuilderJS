75c054b7aac4400173d5a655428d0cc5c269cf2f
akent <alexanderkent099@gmail.com>
2024-02-01 14:04:34 -0500

    Implement main function for SVG generation and saving to file

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ const { Circle, Triangle, Square } = require('./lib/shapes');
 
 async function promptUser() {
     return inquirer.prompt([
-       {
-        type: 'input', 
-        name: 'text',
-        message: 'Enter up to three characters for the logo:',
-        validate: input => input.lenth <= 3
-       },
-       {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter a color for the text:',
-        default: 'black'
-       },
-       {
-        type: 'list', 
-        name: 'shape', 
-        message: 'Which shape would you like to use?', 
-        choices: ['Circle', 'Triangle', 'Square'],
-       },
-       {
-        type: 'input',
-        name: 'shapeColor', 
-        message: 'What color would you like your shape to be?',
-        default: 'blue',
-       } 
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the logo:',
+            validate: input => input.length <= 3
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a color for the text:',
+            default: 'black'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Which shape would you like to use?',
+            choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color would you like your shape to be?',
+            default: 'blue',
+        }
     ]);
 }
 
@@ -50,7 +50,26 @@ function generateSVG(shapeName, shapeColor, text, textColor) {
     }
 
     shape.setColor(shapeColor);
-    shapeColor.setText(text, textColor);
+    shape.setText(text, textColor);
 
     return shape.render();
 }
+
+function saveSVGToFile(svgString) {
+    const filePath = path.join(__dirname, 'logo.svg');
+    fs.writeFileSync(filePath, svgString);
+    console.log('Generated logo.svg');
+}
+
+async function main() {
+    try {
+        const userInput = await promptUser();
+        const svg = generateSvg(userInput.shape, userInput.shapeColor, userInput.text, userInput.textColor);
+        saveSVGToFile(svg);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+main();
+
